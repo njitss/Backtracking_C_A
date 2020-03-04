894372be263781678a3ff972080ba6faa8df588b
Joram Sjamaar <47634296+joram-sjamaar@users.noreply.github.com>
2020-03-04 15:58:33 +0100

    Added a simple maze and test for it

--- src/tests/MazeTests.java
+++ src/tests/MazeTests.java
@@ -2,16 +2,12 @@ package tests;
 
 import main.exceptions.MazeIsEmptyException;
 import main.models.Maze;
-import main.models.Node;
-import main.models.Pawn;
 import main.models.State;
-import main.utils.Color;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class MazeTests {
@@ -30,7 +26,6 @@ public class MazeTests {
 
     @Test
     public void totalStepsShouldBe37() throws MazeIsEmptyException {
-
         maze.importMaze("maze.json");
 
         // Run and store the result.
@@ -39,14 +34,21 @@ public class MazeTests {
         Assertions.assertEquals(37, result.size());
     }
 
-    // Let op, Als deze loopt dan werkt de eerste test niet...
     @Test()
     public void emptyMazeShouldThrowException() {
-
-        Assertions.assertThrows(MazeIsEmptyException.class, () -> maze.importMaze("empty_maze.json"));
+        Assertions.assertThrows(MazeIsEmptyException.class, () ->
+            maze.importMaze("empty_maze.json")
+        );
     }
 
-    // TODO: Simpele maze met 1 pad
+    @Test
+    public void simpleMazeShouldTraverse() throws MazeIsEmptyException {
+        maze.importMaze("simple_maze.json");
 
+        // Run and store the result.
+        List<State> result = maze.run();
+
+        Assertions.assertEquals(2, result.size());
+    }
 
 }
